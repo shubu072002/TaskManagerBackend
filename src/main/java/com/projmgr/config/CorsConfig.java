d3cf91ecc080b4fc778d8557c3f1eacc1672feed
shubu072002 <shubhampatel.4124@gmai.com>
2026-05-03 08:18:04 +0100

    cors

--- src/main/java/com/projmgr/config/CorsConfig.java
+++ src/main/java/com/projmgr/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://taskfrontend-production.up.railway.app")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
